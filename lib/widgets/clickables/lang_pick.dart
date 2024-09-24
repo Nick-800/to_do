c58b889399c663a9ed8f9d30673e08642d9f325d
Nick-800 <sohaibkmsh@gmail.com>
2024-09-24 14:50:00 +0200

    Added dark mode to LangPick

--- lib/widgets/clickables/lang_pick.dart
+++ lib/widgets/clickables/lang_pick.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:to_do/providers/dark_mode_provider.dart';
 import 'package:to_do/providers/language_provider.dart';
 
 class LangPick extends StatelessWidget {
@@ -12,29 +13,39 @@ class LangPick extends StatelessWidget {
   String dropdownvalue = "en";
 
   List<String> langs = ["ar", "en", "es"];
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<LanguageProvider>(builder: (context, lp, _) {
+    return Consumer2<LanguageProvider, DarkModeProvider>(builder: (context, lp,dm, _) {
       return Column(
         children: [
           Container(
+            width: 80,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8),
-              color: Colors.grey[300],
+              color:dm.isDark ? Colors.blueGrey : Colors.grey[300],
+              border: Border.all(color: dm.isDark ? Colors.blueGrey : Colors.grey, width: 1),
             ),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
             child: DropdownButton(
               value: lp.lang,
-              icon: const Icon(Icons.keyboard_arrow_down),
-              items: langs.map((String langs) {
+              icon:  Icon(Icons.keyboard_arrow_down,
+                  color: dm.isDark ? Colors.white: Colors.grey),
+              iconSize: 24,
+              style: TextStyle(color: dm.isDark ? Colors.white: Colors.grey[800], fontSize: 16),
+              items: langs.map((String lang) {
                 return DropdownMenuItem(
-                  value: langs,
-                  child: Text(langs),
+                  value: lang,
+                  child: Text(lang),
                 );
               }).toList(),
               onChanged: (String? newValue) {
                 dropdownvalue = newValue!;
                 lp.switchLanguage(newValue);
               },
+              dropdownColor:dm.isDark ? Colors.blueGrey :  Colors.white,
+              focusColor: Colors.grey[300],
+              elevation: 4,
             ),
           )
         ],
